633f4f8ad8b6205c200d26e9ceafd415ebfa7bf6
Natalia Salpea <48295759+nattienat09@users.noreply.github.com>
2020-08-31 16:38:08 +0300

    Add files via upload

---
+++ flavo.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+class flavo:
+    def __init__(self,xs,ys,length,theta,random_x,random_y,alive = 'yes'):
+        self.xs = xs
+        self.ys = ys
+        self.length = length
+        self.theta = theta
+        self.random_x = random_x
+        self.random_y = random_y
+        self.alive = alive
